a0ad7cb0eac7d37216822b7565b772fa36adf0ac
Ponttunen <mymememail2018@protonmail.com>
2025-11-08 13:19:50 +0200

    Muokattu kohta 1. mukaisesti pylint-säännöt ja koodi toimimaan muutettujen pylint-sääntöjen mukaan.

--- src/index.py
+++ src/index.py
@@ -1,10 +1,16 @@
 from varasto import Varasto
 
-
 def main():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
+    get_set_prints(mehua, olutta)
+    virhetilanteita_varasto()
+    olut_ja_mehuvarasto_virheet(mehua, olutta)
+    mehu_ja_olutvaraston_virheet(mehua, olutta)
+    foo()
 
+
+def get_set_prints(mehua, olutta):
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
@@ -22,6 +28,8 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+
+def virhetilanteita_varasto():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
@@ -31,6 +39,8 @@ def main():
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+
+def olut_ja_mehuvarasto_virheet(mehua, olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
@@ -41,6 +51,8 @@ def main():
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
 
+
+def mehu_ja_olutvaraston_virheet(mehua, olutta):
     print(f"Olutvarasto: {olutta}")
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
@@ -52,6 +64,14 @@ def main():
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
+    for i in "banana":
+        for baz in "split":
+            print(i + baz)
+
+
+def foo():
+    bar = 13
+    print(bar)
 
 
 if __name__ == "__main__":
